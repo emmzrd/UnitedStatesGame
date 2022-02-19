6962814267a09ed09054468f5c3a4a4064f1b4fd
emmzrd <61377267+emmzrd@users.noreply.github.com>
2022-02-19 16:20:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from turtle import Turtle, Screen
+# from state import State
+import pandas
+
+
+tim = Turtle()
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+tim.shape(image)
+score = 0
+
+
+def add_correct_state():
+    new_state = Turtle()
+    new_state.hideturtle()
+    new_state.penup()
+    new_state.color("Black")
+    new_state.goto(x_axis, y_axis)
+    new_state.write(answer_state)
+
+def win():
+    winner = Turtle()
+    winner.hideturtle()
+    winner.penup()
+    winner.color("Black")
+    winner.goto(0, 0)
+    winner.write("You Win!")
+
+
+data = pandas.read_csv("50_states.csv")
+state_list = data["state"].to_list()
+
+game_is_on = True
+
+while game_is_on:
+
+    answer_state = screen.textinput(title="Guess the State." f" Score: {score}/50", prompt="What's another state's name?").title()
+    print(answer_state)
+
+    state_row = data[data.state == answer_state]
+    state_row_dataframe = state_row.values.tolist()
+    x_axis = int(state_row_dataframe[0][1])
+    y_axis = int(state_row_dataframe[0][2])
+
+    print(state_row)
+    print(state_list)
+    print(x_axis)
+    print(y_axis)
+
+    new_list = []
+    for state in state_list:
+        if state == answer_state:
+            add_correct_state()
+            score += 1
+            state_list.remove(state)
+            if score == 50:
+                win()
+                game_is_on = False
+
+screen.exitonclick()
